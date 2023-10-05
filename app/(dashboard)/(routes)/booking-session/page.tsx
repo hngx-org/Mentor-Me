98de15dccf26359c3032dcee9275f3bd685e2b19
Vxrcel <skyeparagon@gmail.com>
2023-10-05 17:53:34 +0100

    Added more pages

---
+++ app/(dashboard)/(routes)/booking-session/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function BookinSession() {
+	return <div>Bookin Session Page</div>;
+}
---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,19 @@
+import Navbar from "@/components/sections/Navbar";
+import Sidebar from "@/components/sections/Sidebar";
+
+const DashboardLayout = async ({ children }: { children: React.ReactNode }) => {
+	return (
+		<div>
+			<div>
+				<Sidebar />
+			</div>
+			<main>
+				<Navbar />
+
+				{children}
+			</main>
+		</div>
+	);
+};
+
+export default DashboardLayout;
