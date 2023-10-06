f4be83e6571edacc69efee85cbd25b31c3fb9f7f
Tania <38540885+crystal4000@users.noreply.github.com>
2023-10-06 09:59:08 -0500

    Created Mentor-Profile-Verification

---
+++ app/(mentor)/profile-verification/page.tsx
@@ -0,0 +1,10 @@
+import RootLayout from "@/app/layout";
+import React from "react";
+
+export default function MentorProfileVerification() {
+  return (
+    <div>
+      <div>MentorProfileVerification</div>
+    </div>
+  );
+}
