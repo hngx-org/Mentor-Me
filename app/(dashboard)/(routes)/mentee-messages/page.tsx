570b295a06e6466e74f34038da38cfc61688fb9d
Richard Eberechi Uche <eb3rechi@gmail.com>
2023-10-06 18:58:29 +0100

    fix

--- app/(dashboard)/(routes)/mentee-messages/page.tsx
+++ app/(dashboard)/(routes)/mentee-messages/page.tsx
@@ -1,7 +1,4 @@
 import React from "react";
-import MessageUserProfile from "./components/message/UserProfile";
-import MessagesLayout from "./components/message/MessagesLayout";
-
 export default function MenteeMessages() {
   return <div>Messages Page</div>;
 }
