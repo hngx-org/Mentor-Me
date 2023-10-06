e7a9dd0c262530ffee1efa3526aae9d7e2cdc893
goodness <goodyc474@gmail.com>
2023-10-06 09:50:32 +0100

    Feat: complete  button component

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,13 +1,44 @@
 import Footer from "@/components/sections/Footer";
 import HeaderAfterSignUp from "@/components/sections/HeaderAfterSignUp";
 import HeaderBeforeSignUp from "@/components/sections/HeaderBeforeSignUp";
+import Button from "@/components/ui/Button";
+import Shoppimg from "./../../public/assets/shopping-cart.png";
+import Image from "next/image";
 
 export default function Landing() {
   return (
-    <>
-      <HeaderAfterSignUp />
-      <HeaderBeforeSignUp />
-      <Footer />
-    </>
+    <div className="flex gap-6 px-4 py-4">
+      <Button
+        className="p-3"
+        paddingLess={false}
+        iconPresent={false}
+        variant="primary"
+        type="button"
+      >
+        <div>Button</div>
+      </Button>
+
+      <Button
+        className="p-3"
+        paddingLess={false}
+        iconPresent={false}
+        variant="secondary"
+        type="button"
+      >
+        <div className="flex   justify-center gap-2">
+          <Image src={Shoppimg} alt="" width={20} height={20} />
+          Button
+        </div>
+      </Button>
+      <Button
+        className="p-3"
+        paddingLess={false}
+        iconPresent={false}
+        variant="disabled-primary"
+        type="button"
+      >
+        <div className="flex   justify-center gap-2">Button</div>
+      </Button>
+    </div>
   );
 }
--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -1,5 +1,58 @@
+import Image, { StaticImageData } from "next/image";
 import React from "react";
 
-export default function Button() {
-  return <div>Button</div>;
+type ButtonProps = {
+  className: string;
+  children: React.ReactNode;
+  variant: string;
+  square?: string;
+  paddingLess?: boolean;
+  type: string;
+  onClick?: () => void;
+  imgSrc?: StaticImageData;
+  imgAlt?: string | undefined;
+  iconPresent: boolean;
+};
+
+const getVariant = (variant: string) => {
+  switch (variant) {
+    case "primary":
+      return "bg-[#121212]  text-[#FFFFFF]  text-[16px] font-[500]   hover:bg-[#2A2A2A]  hover:text-[#FFFF] rounded-[6px] sm:rounded-[8px]   font-inter";
+    case "secondary":
+      return "bg-[#FFFF]  text-[#000] text-[16px] font-[500]  border-[1.5px]  border-[#121212]  rounded-[6px] sm:rounded-[8px]    font-inter";
+
+    case "disabled-primary":
+      return "bg-[#CCCCCC] text-[#808080]  font-inetr  text-[16px]  rounded-[6px] sm:rounded-[8px]  font-[500]";
+
+    case "disabled-secondary":
+      return "bg-[#808080] text-[#808080]  font-inetr  text-[16px]  border-[1.5px] font-[500] rounded-[6px] sm:rounded-[8px] ";
+
+    default:
+      return "bg-[#121212]-500 hover:bg-violet-700 text-white shadow shadow-violet-600/25 hover:shadow-violet-600/75";
+  }
+};
+
+export default function Button({
+  children,
+  className,
+  variant,
+  square,
+  paddingLess,
+  onClick,
+
+  type,
+  iconPresent,
+  ...props
+}: ButtonProps) {
+  return (
+    <button
+      {...props}
+      className={`${getVariant(variant)} ${
+        !paddingLess &&
+        "xl:w-[140px] xl:h-[42px]  md:h-[40px] md:w-[100px] w-[120px] h-[40px] "
+      }  `}
+    >
+      {children}
+    </button>
+  );
 }
