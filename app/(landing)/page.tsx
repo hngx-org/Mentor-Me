3ef951887106dcaf15039cc1264a3262b732d057
Ezerike Caleb Chikere <cezerike@gmail.com>
2023-10-07 07:57:28 +0100

    "feat: setup my review screen"

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,12 +1,14 @@
 import Footer from "@/components/sections/Footer";
 import HeaderAfterSignUp from "@/components/sections/HeaderAfterSignUp";
 import HeaderBeforeSignUp from "@/components/sections/HeaderBeforeSignUp";
+import ReviewScreen from "@/components/sections/ReviewScreen";
 
 export default function Landing() {
   return (
     <>
       <HeaderAfterSignUp />
       <HeaderBeforeSignUp />
+      <ReviewScreen />
       <Footer />
     </>
   );
