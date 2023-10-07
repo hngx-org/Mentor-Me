5be91ad454449ef6cedbe58bdfb479e7b5986463
Vxrcel <skyeparagon@gmail.com>
2023-10-07 17:35:51 +0100

    Working on Mentee profile

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,8 +12,8 @@ const config: Config = {
 				tall: { raw: "(max-height: 1000px)" },
 			},
 			colors: {
-				Neutral60: "#2A2A2A",
 				NeutalBase: "#020202",
+				Neutral60: "#2A2A2A",
 				Neutra50: "#2c2c2c",
 				Neutra40: "#565656",
 				Neutra30: "#808080",
