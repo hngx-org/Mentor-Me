39d2270406513161fc215ff538b5d4c43ecfc7df
Vxrcel <skyeparagon@gmail.com>
2023-10-07 11:58:20 +0100

    Refactoring Mentor Sidebar

--- components/sections/SideBars/SidebarMentor.tsx
+++ components/sections/SideBars/SidebarMentor.tsx
@@ -1,199 +1,105 @@
 "use client";
-import { SideBarIcons } from "@/public/assets/Icons";
+import { sidebarMentorLinks } from "@/constants/constants";
+import { SideBarIcons } from "@/public/assets/icons";
+import Link from "next/link";
 
 export type SideBarMentorProps = {
-  light?: boolean;
+	light?: boolean;
 };
 
-export default function SidebarMentor({ light = true }: SideBarMentorProps) {
-  return (
-    <section
-      className={`hidden w-[274px]  px-3 min-h-screen h-full fixed lg:flex left-0 top-0 border-2 ${
-        light ? "bg-[#fff]" : " bg-[#000]"
-      }`}
-    >
-      <div className="flex flex-col w-full">
-        <div className={`${light ? "h-[70px]" : "h-[82px]"}  py-4`}>
-          {light ? SideBarIcons.logo2() : SideBarIcons.LogoIcon()}
-        </div>
+export default function SidebarMentor({
+	light = false,
+	path,
+}: SideBarMentorProps & { path?: string | null | undefined }) {
+	return (
+		<section
+			className={`hidden w-[274px]  p-5 min-h-screen h-full fixed lg:flex left-0 top-0 ${
+				light ? "bg-[#fff]" : " bg-[#000]"
+			}`}
+		>
+			<div className="flex flex-col justify-between h-full">
+				{/* menu */}
 
-        {/* menu */}
+				<div>
+					<div className="w-full pl-3">
+						{light ? SideBarIcons.logo2() : SideBarIcons.LogoIcon()}
+					</div>
+					<div className="mt-20">
+						<p className="  font-Inter tetx-[14px]  leading-[20.3px] font-[500]   text-Neutra30 pl-3">
+							MENU
+						</p>
+						<ul className=" px-3  py-4 flex flex-col gap-4 cursor-pointer text-xl">
+							{sidebarMentorLinks.map((link) => (
+								<Link key={link.id} href={link.path} prefetch>
+									<li
+										className={`flex gap-3  ${
+											light && path === link.label.toLowerCase()
+												? "bg-[#E5FFFF]"
+												: !light && path === link.label.toLowerCase()
+												? " bg-Neutra50"
+												: ""
+										} rounded-[5px] p-2 items-center`}
+									>
+										<span>{light ? link.iconLight : link.iconDark}</span>
+										<span
+											className={` font-Inter tetx-[14px] font-[500] ${
+												light ? "text-[#008080]" : "text-[#fff]"
+											} `}
+										>
+											{link.label}
+										</span>
+									</li>
+								</Link>
+							))}
+						</ul>
+					</div>
+				</div>
+				{/* logout */}
 
-        <div className="mt-5 ">
-          <p className="  font-Inter tetx-[14px]  leading-[20.3px] font-[500]   text-Neutra30">
-            MENU
-          </p>
-          <ul
-            className={` px-6  py-4 flex flex-col  cursor-pointer ${
-              light ? "gap-1" : ""
-            }`}
-          >
-            <li
-              className={`flex gap-3  ${
-                light ? "bg-[#E5FFFF]" : " bg-Neutra50"
-              } rounded-[5px] p-2 items-center`}
-            >
-              <span>
-                {light ? SideBarIcons.homeIcon2() : SideBarIcons.homeIcon()}
-              </span>
-              <span
-                className={` font-Inter text-[10px] font-[500] ${
-                  light ? "text-[#008080]" : "text-[#fff]"
-                } `}
-              >
-                Home
-              </span>
-            </li>
-            <li className="flex  gap-3 rounded-[5px] p-2 items-center ">
-              <span>
-                {light
-                  ? SideBarIcons.messaging2()
-                  : SideBarIcons.messagesIcon()}
-              </span>
-              <span
-                className={` font-Inter text-[10px] font-[500] ${
-                  light ? "text-[#40444B]" : "text-[#fff]"
-                } `}
-              >
-                Messages
-              </span>
-            </li>
-            <li className="flex  gap-3 rounded-[5px] p-2 items-center ">
-              <span>
-                {light
-                  ? SideBarIcons.MyMentors2()
-                  : SideBarIcons.myMentorsIcons()}
-              </span>
-              <span
-                className={` font-Inter text-[10px] font-[500] ${
-                  light ? "text-[#40444B]" : "text-[#fff]"
-                } `}
-              >
-                My Mentors
-              </span>
-            </li>
-            <li className="flex  gap-3  rounded-[5px] p-2 items-center">
-              <span>
-                {light ? SideBarIcons.session2() : SideBarIcons.sessions()}
-              </span>
-              <span
-                className={` font-Inter text-[10px] font-[500] ${
-                  light ? "text-[#40444B]" : "text-[#fff]"
-                } `}
-              >
-                Sessions
-              </span>
-            </li>
-            <li className="flex  gap-3 rounded-[5px] p-2  items-center">
-              <span>
-                {light
-                  ? SideBarIcons.resources2()
-                  : SideBarIcons.resourcesIcon()}
-              </span>
-              <span
-                className={` font-Inter text-[10px] font-[500] ${
-                  light ? "text-[#40444B]" : "text-[#fff]"
-                } `}
-              >
-                Resources
-              </span>
-            </li>
-            <li className="flex  gap-3  rounded-[5px] p-2 items-center">
-              <span>
-                {light
-                  ? SideBarIcons.communities2()
-                  : SideBarIcons.communitiesIcon()}
-              </span>
-              <span
-                className={` font-Inter text-[10px] font-[500] ${
-                  light ? "text-[#40444B]" : "text-[#fff]"
-                } `}
-              >
-                Communities
-              </span>
-            </li>
-            <li className="flex  gap-3  rounded-[5px] p-2 items-center">
-              <span>
-                {light ? SideBarIcons.earning2() : SideBarIcons.earning()}
-              </span>
-              <span
-                className={` font-Inter text-[10px] font-[500] ${
-                  light ? "text-[#40444B]" : "text-[#fff]"
-                } `}
-              >
-                Earnings
-              </span>
-            </li>
-            {light ? (
-              ""
-            ) : (
-              <li className="flex  gap-3  rounded-[5px] p-2 items-center">
-                <span>{SideBarIcons.plans()}</span>
-                <span
-                  className={` font-Inter text-[10px] font-[500] ${
-                    light ? "text-[#40444B]" : "text-[#fff]"
-                  } `}
-                >
-                  Plans
-                </span>
-              </li>
-            )}
-            {light ? (
-              ""
-            ) : (
-              <li className="flex  gap-3  rounded-[5px] p-2 items-center">
-                <span>{SideBarIcons.reviews()}</span>
-                <span
-                  className={` font-Inter text-[10px] font-[500] ${
-                    light ? "text-[#40444B]" : "text-[#fff]"
-                  } `}
-                >
-                  Reviews
-                </span>
-              </li>
-            )}
-          </ul>
-        </div>
+				<div className="my-6 border-t-2 border-Neutra40 ">
+					<ul className=" px-3  py-1 flex flex-col  cursor-pointer">
+						<Link href="/mentee-settings?path=settings">
+							<li
+								className={`flex gap-2 rounded-[5px] p-2 items-center ${
+									light && path === "settings"
+										? "bg-[#E5FFFF]"
+										: !light && path === "settings"
+										? " bg-Neutra50"
+										: ""
+								}`}
+							>
+								<span>{SideBarIcons.settingIcon()}</span>
+								<span
+									className={` font-Inter tetx-[11px] font-[500]  rounded-[5px] p-2 text-white`}
+								>
+									Setting
+								</span>
+							</li>
+						</Link>
+						<li className={`flex gap-3  rounded-[5px] p-2`}>
+							<span>{SideBarIcons.logoutIcon()}</span>
+							<span className="  font-Inter tetx-[11px] font-[500]  text-Error50">
+								LogOut
+							</span>
+						</li>
+					</ul>
+				</div>
+				{/* profile */}
 
-        {/* logout */}
-
-        <div className={`${light ? "my-3" : ""} border-t-2 border-Neutra40 `}>
-          <ul className=" px-3  py-2 flex flex-col gap-2 cursor-pointer">
-            <li className={`flex gap-2 rounded-[5px] p-2 items-center`}>
-              <span>{SideBarIcons.settingIcon2()}</span>
-              <span
-                className={` font-Inter text-[10px] font-[500] ${
-                  light ? "text-[#40444B]" : "text-[#fff]"
-                } `}
-              >
-                Setting
-              </span>
-            </li>
-            <li className={`flex gap-3  rounded-[5px] p-2 items-center`}>
-              <span>{SideBarIcons.logoutIcon()}</span>
-              <span className="  font-Inter text-[10px] font-[500]  text-Error50">
-                LogOut
-              </span>
-            </li>
-          </ul>
-        </div>
-        {/* profile */}
-
-        <div className="bottom-0">
-          <ul className="  cursor-pointer   ">
-            <li className={`flex gap-3 items-center  p-2`}>
-              <span>{SideBarIcons.profileIcon()}</span>
-              <span className="  font-Inter text-[10px] font-[500]   text-Neutra30">
-                <span className={`${light ? "text-[#000]" : " text-Neutra30"}`}>
-                  {" "}
-                  Funmi Oladapo
-                </span>
-                <br /> funmi@zurimp.com
-              </span>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </section>
-  );
+				<div className="  bottom-0">
+					<ul className="  cursor-pointer   ">
+						<li className={`flex gap-3 items-center  p-2`}>
+							<span>{SideBarIcons.profileIcon()}</span>
+							<span className="  font-Inter tetx-[10px] font-[500]   text-Neutra30">
+								<span className={`${light ? "text-[#000]" : " text-Neutra30"}`}>
+									Funmi Oladapo
+								</span>
+								<br /> funmi@zurimp.com
+							</span>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</section>
+	);
 }
--- constants/constants.ts
+++ constants/constants.ts
@@ -1,54 +1,126 @@
-import { SideBarIcons } from "@/public/assets/Icons";
-export type SidebarMenteeLinksProps = {
-  path: string;
-  label: string;
-  iconLight?: React.ReactNode;
-  iconDark?: React.ReactNode;
-  id: number;
+import { SideBarIcons } from "@/public/assets/icons";
+export type SidebarLinksProps = {
+	path: string;
+	label: string;
+	iconLight?: React.ReactNode;
+	iconDark?: React.ReactNode;
+	id: number;
 };
 
-export const sidebarMenteeLinks: SidebarMenteeLinksProps[] = [
-  {
-    id: 1,
-    path: "/mentee-profile?path=home",
-    label: "Home",
-    iconDark: SideBarIcons.homeIcon(),
-    iconLight: SideBarIcons.homeIcon2(),
-  },
+export const sidebarMenteeLinks: SidebarLinksProps[] = [
+	{
+		id: 1,
+		path: "/mentee-profile?path=home",
+		label: "Home",
+		iconDark: SideBarIcons.homeIcon(),
+		iconLight: SideBarIcons.homeIcon2(),
+	},
 
-  {
-    id: 2,
-    path: "/mentee-messages?path=messages",
-    label: "Messages",
-    iconDark: SideBarIcons.messagesIcon(),
-    iconLight: SideBarIcons.messaging2(),
-  },
-  {
-    id: 3,
-    path: "/mentee-booking?path=bookings",
-    label: "Bookings",
-    iconDark: SideBarIcons.bookingIcon(),
-    iconLight: SideBarIcons.booking2(),
-  },
-  {
-    id: 4,
-    path: "/mentee-sessions?path=sessions",
-    label: "Sessions",
-    iconDark: SideBarIcons.sessions(),
-    iconLight: SideBarIcons.session2(),
-  },
-  {
-    id: 5,
-    path: "/mentee-resources?path=resources",
-    label: "Resources",
-    iconDark: SideBarIcons.resourcesIcon(),
-    iconLight: SideBarIcons.resources2(),
-  },
-  {
-    id: 6,
-    path: "/mentee-communities?path=communities",
-    label: "Communities",
-    iconDark: SideBarIcons.communitiesIcon(),
-    iconLight: SideBarIcons.communities2(),
-  },
+	{
+		id: 2,
+		path: "/mentee-messages?path=messages",
+		label: "Messages",
+		iconDark: SideBarIcons.messagesIcon(),
+		iconLight: SideBarIcons.messaging2(),
+	},
+	{
+		id: 3,
+		path: "/mentee-booking?path=bookings",
+		label: "Bookings",
+		iconDark: SideBarIcons.bookingIcon(),
+		iconLight: SideBarIcons.booking2(),
+	},
+	{
+		id: 4,
+		path: "/mentee-sessions?path=sessions",
+		label: "Sessions",
+		iconDark: SideBarIcons.sessions(),
+		iconLight: SideBarIcons.session2(),
+	},
+	{
+		id: 5,
+		path: "/mentee-resources?path=resources",
+		label: "Resources",
+		iconDark: SideBarIcons.resourcesIcon(),
+		iconLight: SideBarIcons.resources2(),
+	},
+	{
+		id: 6,
+		path: "/mentee-communities?path=communities",
+		label: "Communities",
+		iconDark: SideBarIcons.communitiesIcon(),
+		iconLight: SideBarIcons.communities2(),
+	},
+];
+
+export const sidebarMentorLinks: SidebarLinksProps[] = [
+	{
+		id: 1,
+		path: "/mentor-profile?path=home",
+		label: "Home",
+		iconDark: SideBarIcons.homeIcon(),
+		iconLight: SideBarIcons.homeIcon2(),
+	},
+
+	{
+		id: 2,
+		path: "/mentor-messages?path=messages",
+		label: "Messages",
+		iconDark: SideBarIcons.messagesIcon(),
+		iconLight: SideBarIcons.messaging2(),
+	},
+	{
+		id: 3,
+		path: "/mentor-mentors?path=MyMentors",
+		label: "My Mentors",
+		iconDark: SideBarIcons.myMentorsIcons(),
+		iconLight: SideBarIcons.MyMentors2(),
+	},
+	{
+		id: 4,
+		path: "/mentor-booking?path=bookings",
+		label: "Bookings",
+		iconDark: SideBarIcons.bookingIcon(),
+		iconLight: SideBarIcons.booking2(),
+	},
+	{
+		id: 5,
+		path: "/mentor-sessions?path=sessions",
+		label: "Sessions",
+		iconDark: SideBarIcons.sessions(),
+		iconLight: SideBarIcons.session2(),
+	},
+	{
+		id: 6,
+		path: "/mentor-resources?path=resources",
+		label: "Resources",
+		iconDark: SideBarIcons.resourcesIcon(),
+		iconLight: SideBarIcons.resources2(),
+	},
+	{
+		id: 7,
+		path: "/mentor-communities?path=communities",
+		label: "Communities",
+		iconDark: SideBarIcons.communitiesIcon(),
+		iconLight: SideBarIcons.communities2(),
+	},
+	{
+		id: 8,
+		path: "/mentor-earnings?path=earnings",
+		label: "Earnings",
+		iconDark: SideBarIcons.earning(),
+		iconLight: SideBarIcons.earning2(),
+	},
+	{
+		id: 9,
+		path: "/mentor-plans?path=plans",
+		label: "Plans",
+		iconDark: SideBarIcons.plans(),
+	},
+	{
+		id: 10,
+		path: "/mentor-reviews?path=reviews",
+		label: "Reviews",
+		iconDark: SideBarIcons.reviews(),
+	},
 ];
