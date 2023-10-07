2befcfa26d44403f8c15ee1eb5f3c2c4a2b789e8
Richard Eberechi Uche <eb3rechi@gmail.com>
2023-10-07 14:40:31 +0100

    update icons

--- public/assets/Icons/index.tsx
+++ public/assets/Icons/index.tsx
@@ -1255,6 +1255,39 @@ const StopIcon = () => {
   );
 };
 
+const VideoIcon = () => {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M12.53 20.42H6.21C3.05 20.42 2 18.32 2 16.21V7.78999C2 4.62999 3.05 3.57999 6.21 3.57999H12.53C15.69 3.57999 16.74 4.62999 16.74 7.78999V16.21C16.74 19.37 15.68 20.42 12.53 20.42Z"
+        stroke="#292D32"
+        stroke-width="1.25"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M19.5202 17.1L16.7402 15.15V8.84001L19.5202 6.89001C20.8802 5.94001 22.0002 6.52001 22.0002 8.19001V15.81C22.0002 17.48 20.8802 18.06 19.5202 17.1Z"
+        stroke="#292D32"
+        stroke-width="1.25"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M11.5 11C12.3284 11 13 10.3284 13 9.5C13 8.67157 12.3284 8 11.5 8C10.6716 8 10 8.67157 10 9.5C10 10.3284 10.6716 11 11.5 11Z"
+        stroke="#292D32"
+        stroke-width="1.25"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+};
 export const SessionIcon = {
   CallIcon,
   StopIcon,
@@ -1263,6 +1296,7 @@ export const SessionIcon = {
   MicrophoneIcon,
   CameraIcon,
   DocumentIcon,
+  VideoIcon,
 };
 export const SideBarIcons = {
   homeIcon,
