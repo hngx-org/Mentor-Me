466d0c5fb42b980d6cb3802039229b8be5f56aca
Tania <38540885+crystal4000@users.noreply.github.com>
2023-10-06 23:13:49 -0500

    Changes to SideBarMentor Page
    changed tetx to text for the classes

--- app/(mentor)/profile-verification/page.tsx
+++ app/(mentor)/profile-verification/page.tsx
@@ -22,7 +22,7 @@ import {
 
 export default function MentorProfileVerification() {
   const [step, setStep] = useState(0);
-  const [verificationStatus, setVerificationStatus] = useState("rejected");
+  const [verificationStatus, setVerificationStatus] = useState("");
   const [showModal, setShowModal] = useState(false);
 
   const handleNextStep = () => {
